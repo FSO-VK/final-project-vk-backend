a0184e3e5b87144b10dd9ef63214a366fc3b3c7a
Olgmuzalev13 <121461949+Olgmuzalev13@users.noreply.github.com>
2025-10-20 16:27:31 +0300

    fix (medication/presentation/http/router.go): добавлен medication в роутинг (#17)
    * fix (FSO-reverse): доабвить medication
    
    * fix (FSO-reverse): убрать s

--- internal/medication/presentation/http/router.go
+++ internal/medication/presentation/http/router.go
@@ -11,10 +11,10 @@ func Router(
 ) *mux.Router {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/all", medicationHandlers.GetMedicationList).Methods("GET")
-	r.HandleFunc("", medicationHandlers.AddMedication).Methods("POST")
-	r.HandleFunc("/{id}", medicationHandlers.UpdateMedication).Methods("PUT")
-	r.HandleFunc("/{id}", medicationHandlers.DeleteMedication).Methods("DELETE")
+	r.HandleFunc("/medication/all", medicationHandlers.GetMedicationList).Methods("GET")
+	r.HandleFunc("/medication", medicationHandlers.AddMedication).Methods("POST")
+	r.HandleFunc("/medication/{id}", medicationHandlers.UpdateMedication).Methods("PUT")
+	r.HandleFunc("/medication/{id}", medicationHandlers.DeleteMedication).Methods("DELETE")
 
 	panicMiddleware := httph.NewPanicRecoveryMiddleware()
 	r.Use(panicMiddleware.Middleware)
