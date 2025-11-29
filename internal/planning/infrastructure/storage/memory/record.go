bb438ea41fc18b29a0771c18501a7a469d249963
Oleg Muzalev <razrushitelyvselennoy@mail.ru>
2025-11-29 17:41:29 +0300

    feat (FSO-249): storage для records

---
+++ internal/planning/infrastructure/storage/memory/record.go
@@ -0,0 +1,86 @@
+package memory
+
+import (
+	"context"
+	"sync"
+
+	"github.com/FSO-VK/final-project-vk-backend/internal/planning/domain/record"
+	"github.com/FSO-VK/final-project-vk-backend/internal/utils/cache"
+	"github.com/google/uuid"
+)
+
+// RecordStorage is a storage for Records.
+type RecordStorage struct {
+	data  *cache.Cache[*record.IntakeRecord]
+	count uint
+
+	mu *sync.RWMutex
+}
+
+// NewRecordStorage returns a new RecordStorage.
+func NewRecordStorage() *RecordStorage {
+	return &RecordStorage{
+		data:  cache.NewCache[*record.IntakeRecord](),
+		count: 0,
+		mu:    &sync.RWMutex{},
+	}
+}
+
+// Create creates a new record in memory.
+func (s *RecordStorage) Save(
+	_ context.Context,
+	newRecord *record.IntakeRecord,
+) error {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	s.count++
+	s.data.Set(newRecord.ID().String(), newRecord)
+	return nil
+}
+
+// Create creates a bulk of new records in memory.
+func (s *RecordStorage) SaveBulk(
+	_ context.Context,
+	bulkOfRecords []*record.IntakeRecord,
+) error {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	for _, oneRecord := range bulkOfRecords {
+		s.count++
+		s.data.Set(oneRecord.ID().String(), oneRecord)
+	}
+	return nil
+}
+
+// GetByID returns a record by id.
+func (s *RecordStorage) GetByID(
+	_ context.Context,
+	id uuid.UUID,
+) (*record.IntakeRecord, error) {
+	requestedRecord, ok := s.data.Get(id.String())
+	if !ok {
+		return nil, record.ErrNoRecordFound
+	}
+	return requestedRecord, nil
+}
+
+// UserRecords returns all records by plan id.
+func (s *RecordStorage) GetByPlanID(
+	_ context.Context,
+	userID uuid.UUID,
+) ([]*record.IntakeRecord, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	var result []*record.IntakeRecord
+	for _, oneRecord := range s.data.GetAll() {
+		if oneRecord.PlanID() == userID {
+			result = append(result, oneRecord)
+		}
+	}
+	if len(result) == 0 {
+		return nil, record.ErrNoRecordFound
+	}
+	return result, nil
+}
