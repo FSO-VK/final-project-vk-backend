e1e5d33c7f0b6416892374a7e7635cf69e749432
Olgmuzalev13 <121461949+Olgmuzalev13@users.noreply.github.com>
2025-12-01 16:22:42 +0300

    feat (FSO-255): Отображение расписания приёма applcation с заглушкой … (#63)
    * feat (FSO-255): Отображение расписания приёма applcation с заглушкой клиента
    
    * feat (FSO-255): сдвиг времени создания records
    
    * fix (FSO-255): исправления

---
+++ internal/planning/application/show_schedule.go
@@ -0,0 +1,164 @@
+// Package application is a package for application logic of the planning service.
+package application
+
+import (
+	"context"
+	"time"
+
+	"github.com/FSO-VK/final-project-vk-backend/internal/planning/domain/plan"
+	"github.com/FSO-VK/final-project-vk-backend/internal/planning/domain/record"
+	"github.com/FSO-VK/final-project-vk-backend/internal/utils/validator"
+	"github.com/google/uuid"
+)
+
+// ShowSchedule is an interface for getting a notification.
+type ShowSchedule interface {
+	Execute(
+		ctx context.Context,
+		cmd *ShowScheduleCommand,
+	) (*ShowScheduleResponse, error)
+}
+
+// ShowScheduleService is a service for creating a subscription.
+type ShowScheduleService struct {
+	planningRepo plan.Repository
+	recordsRepo  record.Repository
+	validator    validator.Validator
+	// createdShift is the offset from 00:00 when records are generated.
+	// At 00:00 + createdShift, all records for that day are created. (basically 24h - today creating for the next day)
+	createdShift time.Duration
+}
+
+// NewShowScheduleService returns a new ShowScheduleService.
+func NewShowScheduleService(
+	planningRepo plan.Repository,
+	recordsRepo record.Repository,
+	valid validator.Validator,
+	createdShift time.Duration,
+) *ShowScheduleService {
+	return &ShowScheduleService{
+		planningRepo: planningRepo,
+		recordsRepo:  recordsRepo,
+		validator:    valid,
+		createdShift: createdShift,
+	}
+}
+
+// ShowScheduleCommand is a request to get a plan.
+type ShowScheduleCommand struct {
+	UserID    string `validate:"required,uuid"`
+	StartDate string `validate:"required,datetime=2006-01-02T15:04:05Z07:00"`
+	EndDate   string `validate:"required,datetime=2006-01-02T15:04:05Z07:00"`
+}
+
+// ScheduleTime is an item of the schedules list to be returned.
+type ScheduleTime struct {
+	IntakeRecordID uuid.UUID
+	MedicationID   uuid.UUID
+	MedicationName string
+	AmountValue    float64
+	AmountUnit     string
+	Status         bool // is taken
+	PlannedAt      time.Time
+	TakenAt        time.Time
+}
+
+// ShowScheduleResponse is a response to get a plan.
+type ShowScheduleResponse struct {
+	Schedule []*ScheduleTime
+}
+
+// Execute executes the ShowSchedule command.
+func (s *ShowScheduleService) Execute(
+	ctx context.Context,
+	req *ShowScheduleCommand,
+) (*ShowScheduleResponse, error) {
+	valErr := s.validator.ValidateStruct(req)
+	if valErr != nil {
+		return nil, ErrValidationFail
+	}
+
+	parsedUser, parsedStart, parsedEnd, err := parseInfo(req.UserID, req.StartDate, req.EndDate)
+	if err != nil {
+		return nil, ErrValidationFail
+	}
+
+	userPlans, err := s.planningRepo.UserPlans(ctx, parsedUser)
+	if err != nil {
+		return nil, ErrNoPlan
+	}
+
+	pastScheduleList := make([]*ScheduleTime, 0, len(userPlans))
+	futureScheduleList := make([]*ScheduleTime, 0, len(userPlans))
+
+	for _, p := range userPlans {
+		amountValue, amountUnit := p.Dosage()
+
+		records, err := s.recordsRepo.GetByPlanID(ctx, p.ID())
+		// if there is an error, it means that the plan has no records
+		// because all of them are in the future and we will calculate them after a while
+		if err == nil {
+			for _, record := range records {
+				if !record.PlannedTime().After(parsedEnd) &&
+					!record.PlannedTime().Before(parsedStart) {
+					pastScheduleList = append(pastScheduleList, &ScheduleTime{
+						IntakeRecordID: record.ID(),
+						MedicationID:   p.MedicationID(),
+						MedicationName: "Medication Name", // need client
+						AmountValue:    amountValue,
+						AmountUnit:     amountUnit,
+						Status:         record.IsTaken(),
+						PlannedAt:      record.PlannedTime(),
+						TakenAt:        record.TakenAt(),
+					})
+				}
+			}
+		}
+		// we are calculating all future records that are not created in db
+		futureTimes := p.Schedule(
+			time.Now().Truncate(24*time.Hour).Add(s.createdShift),
+			parsedEnd,
+		)
+
+		for _, t := range futureTimes {
+			futureScheduleList = append(futureScheduleList, &ScheduleTime{
+				IntakeRecordID: uuid.Nil,
+				MedicationID:   p.MedicationID(),
+				MedicationName: "Medication Name", // need client
+				AmountValue:    amountValue,
+				AmountUnit:     amountUnit,
+				Status:         false,
+				PlannedAt:      t,
+				TakenAt:        time.Time{},
+			})
+		}
+	}
+
+	response := &ShowScheduleResponse{
+		Schedule: append(futureScheduleList, pastScheduleList...),
+	}
+	return response, nil
+}
+
+func parseInfo(
+	userID string,
+	startTime string,
+	endTime string,
+) (uuid.UUID, time.Time, time.Time, error) {
+	parsedUser, err := uuid.Parse(userID)
+	if err != nil {
+		return uuid.Nil, time.Time{}, time.Time{}, ErrValidationFail
+	}
+
+	parsedStart, err := time.Parse(time.RFC3339, startTime)
+	if err != nil {
+		return uuid.Nil, time.Time{}, time.Time{}, ErrValidationFail
+	}
+
+	parsedEnd, err := time.Parse(time.RFC3339, endTime)
+	if err != nil {
+		return uuid.Nil, time.Time{}, time.Time{}, ErrValidationFail
+	}
+
+	return parsedUser, parsedStart, parsedEnd, nil
+}
