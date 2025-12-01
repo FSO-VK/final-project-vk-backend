f25a2703d4c958e88420e812d657ff2ca6047302
Olgmuzalev13 <121461949+Olgmuzalev13@users.noreply.github.com>
2025-12-01 16:11:48 +0300

    feat (FSO-260): application слой получения всех планов пользователя (#65)
    * feat (FSO-260): application слой получения всех планов пользователя
    
    * feat (FSO-260): application слой получения всех планов пользователя

--- internal/planning/domain/plan/repository.go
+++ internal/planning/domain/plan/repository.go
@@ -13,6 +13,6 @@ var ErrNoPlanFound = errors.New("plan not found")
 // Repository is a domain service interface for repository.
 type Repository interface {
 	GetByID(ctx context.Context, id uuid.UUID) (*Plan, error)
-	UserPlans(ctx context.Context, userID uuid.UUID) (*[]Plan, error)
+	UserPlans(ctx context.Context, userID uuid.UUID) ([]*Plan, error)
 	Save(ctx context.Context, plan *Plan) error
 }
