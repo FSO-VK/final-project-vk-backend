52a699abfdcea49b86e0ab9a218e5d60db68647c
Oleg Muzalev <razrushitelyvselennoy@mail.ru>
2025-12-16 18:52:16 +0300

    fix (FSO-298): уведомления об истекающем сроке годности

--- internal/medication/infrastructure/config/configs.go
+++ internal/medication/infrastructure/config/configs.go
@@ -7,19 +7,21 @@ import (
 	vidalclient "github.com/FSO-VK/final-project-vk-backend/internal/medication/infrastructure/vidal/client"
 	vidalstorage "github.com/FSO-VK/final-project-vk-backend/internal/medication/infrastructure/vidal/storage/mongo"
 	"github.com/FSO-VK/final-project-vk-backend/internal/medication/presentation/http"
+	notification "github.com/FSO-VK/final-project-vk-backend/internal/utils/notification_client"
 	auth "github.com/FSO-VK/final-project-vk-backend/pkg/auth/client"
 	"github.com/FSO-VK/final-project-vk-backend/pkg/llm/gigachat"
 )
 
 // Config is a configuration for the medication service.
 type Config struct {
-	Server    http.ServerConfig
-	Internal  http.ServerConfig
-	Scan      dataMatrixClient.ClientConfig
-	Auth      auth.ClientConfig
-	Vidal     vidal
-	Gigachat  gigachat.ClientConfig
-	Assistant llm.InstructionAssistantConfig
+	Server       http.ServerConfig
+	Internal     http.ServerConfig
+	Scan         dataMatrixClient.ClientConfig
+	Auth         auth.ClientConfig
+	Vidal        vidal
+	Gigachat     gigachat.ClientConfig
+	Assistant    llm.InstructionAssistantConfig
+	Notification notification.ClientConfig
 }
 
 type vidal struct {
