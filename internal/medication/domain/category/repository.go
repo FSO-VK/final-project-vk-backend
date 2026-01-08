f42d1a25e7b42a5df9246fe279a06da821a65e85
Ilya Andriyanov <ilyaandry35@gmail.com>
2026-01-08 21:32:20 +0300

    ref (FSO-304): убран medication.Category из домена

--- internal/medication/domain/category/repository.go
+++
@@ -1,9 +0,0 @@
-package category
-
-import "context"
-
-// Repository is a domain interface for category's repository.
-type Repository interface {
-	Create(ctx context.Context, category *Category) (*Category, error)
-	GetByID(ctx context.Context, categoryID uint) (*Category, error)
-}
